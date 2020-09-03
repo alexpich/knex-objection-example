ba4abad9f28dcb47420a984fe70431ffc86d33f5
Alex <pich.alex01@gmail.com>
2020-09-03 15:24:26 -0700

    Set up knex

---
+++ server/knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+const credentials = require("./config/db_config");
+
+module.exports = {
+  development: {
+    client: "mysql2",
+    connection: {
+      database: credentials.database,
+      user: credentials.user,
+      password: credentials.password,
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};

07c374c2718f814c16abe8478bcbf42ea0a07be2
Alex <pich.alex01@gmail.com>
2020-09-03 18:29:29 -0700

    Objection.js queries

--- server/routes/api/todos.js
+++ server/routes/api/todos.js
@@ -2,8 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Todo = require("../../models/Todo");
 
+// router.get("/", async (req, res) => {
+//   const todos = await Todo.query();
+//   res.json(todos);
+// });
+
+// router.get("/", async (req, res) => {
+//   const todos = await Todo.query().select("id", "todo", "done");
+//   // Handle if no todos have been found
+//   if (!todos.length === 0) {
+//     return res.status(404).json({ message: "No todo found" });
+//   }
+//   res.json(todos);
+// });
+
 router.get("/", async (req, res) => {
-  const todos = await Todo.query();
+  const todos = await Todo.query().where("todo", "Pay rent");
+  // Handle if no todos have been found
+  if (!todos.length === 0) {
+    return res.status(404).json({ message: "No todo found" });
+  }
+  // If todos exist, return the todo set
   res.json(todos);
 });
 
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User.js");
 
+// router.get("/", async (req, res) => {
+//   // Step 5 from test.js
+//   const users = await User.query();
+//   res.json(users);
+// });
+
 router.get("/", async (req, res) => {
-  // Step 5 from test.js
-  const users = await User.query();
+  const users = await User.query().findById(2); // returns a single user in an object, not in an array
   res.json(users);
 });
 
